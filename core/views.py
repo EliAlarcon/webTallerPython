735d80969ca5b48619f2be80e61b20bd76432f31
EliAlarcon <EliAlarcon@users.noreply.github.com>
2024-01-07 20:23:30 -0500

    first commit

---
+++ core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, "core/home.html")
+
+def about(request):
+    return render(request, "core/about.html")
+
+def ourEcoLodge(request):
+    return render(request, "core/our-eco-lodge.html")
+
+def tour(request):
+    return render(request, "core/tour.html")
+
+def contact(request):
+    return render(request, "core/contact.html")
+
