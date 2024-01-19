1456352913789cc3996c3af78bfab9f7a97e0d0e
EliAlarcon <EliAlarcon@users.noreply.github.com>
2024-01-18 22:08:28 -0500

    Actualización

--- lodges/models.py
+++ lodges/models.py
@@ -20,14 +20,27 @@ class Lodge(models.Model):
     
 
 # Modelos con relaciones a Lodge
-""" class Contenido(models.Model):
+class Section(models.Model):
     name=models.CharField(max_length=100, verbose_name='Título del contenido')
     created=models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
     updated=models.DateTimeField(auto_now=True, verbose_name='Fecha de edición')
     
     class Meta:
-        verbose_name = 'ModelName'
-        verbose_name_plural = 'ModelNames'
+        verbose_name = 'Content'
+        verbose_name_plural = 'Content'
 
     def __str__(self):
-        return  """
+        return 
+    
+class DetailSection(models.Model):
+    content=models.TextField(verbose_name='Contenido')
+    image=models.ImageField(upload_to='lodges', verbose_name='Imagen')
+    created=models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+    updated=models.DateTimeField(auto_now=True, verbose_name='Fecha de edición')
+    
+    class Meta:
+        verbose_name = 'Detail Content'
+        verbose_name_plural = 'Detail Content'
+
+    def __str__(self):
+        return self.content
